e36de72d6b9be70cc347cb1058431a70fb1a9292
gauravprwl14 <gauravprwl14@gmail.com>
2019-02-15 00:50:40 +0530

    setup the test case with typescript

---
+++ __tests__/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import 'react-native'
+import * as React from 'react'
+import App from '../App'
+
+// Note: test renderer must be required after react-native.
+import reactTestRenderer from 'react-test-renderer'
+
+it('renders correctly', () => {
+  reactTestRenderer.create(<App />)
+})
