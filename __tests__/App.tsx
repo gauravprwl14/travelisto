74b5cb627e203b8611c0ad4c75f20f0ae0ff0996
gauravprwl14 <gauravprwl14@gmail.com>
2019-02-17 23:17:36 +0530

    moved the app location inside the js folder

--- __tests__/App.tsx
+++ __tests__/App.tsx
@@ -5,7 +5,7 @@
 
 import 'react-native'
 import * as React from 'react'
-import App from '../App'
+import App from '../js/App'
 
 // Note: test renderer must be required after react-native.
 import reactTestRenderer from 'react-test-renderer'
