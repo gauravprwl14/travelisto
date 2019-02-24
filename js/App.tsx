70d3e42fdabc756c87d5467c0d1d65cb798fc1f1
gauravprwl14 <gauravprwl14@gmail.com>
2019-02-25 00:17:33 +0530

    integrated the redux store with the app

--- js/App.tsx
+++ js/App.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
+import { Provider } from 'react-redux'
 import Navigation from './Navigation'
+import { configureStore } from './store'
+
+const appStore = configureStore()
 
 const MainApp = () => {
-  return (
-    <Navigation />
-  )
+    return (
+        <Provider store={appStore}>
+            <Navigation />
+        </Provider>
+    )
 }
 
 export default MainApp
