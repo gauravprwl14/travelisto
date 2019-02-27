c155a7ed4a5447a648be13d9f456f0051a39f5f4
gauravprwl14 <gauravprwl14@gmail.com>
2019-02-28 01:44:29 +0530

    added the re usable container components

---
+++ js/Components/Container/styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+import * as theme from '../../utils/theme'
+
+const styles = StyleSheet.create({
+    container: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: theme.colors.themeBgColor,
+    },
+})
+
+export default styles
