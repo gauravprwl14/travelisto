98aa4b727a61792f3f59b834c8f6328a539457b6
gauravprwl14 <gauravprwl14@gmail.com>
2019-02-28 23:17:46 +0530

    aligned the container nodes in the vertical center

--- js/Components/Container/styles.tsx
+++ js/Components/Container/styles.tsx
@@ -3,6 +3,7 @@ import * as theme from '../../utils/theme'
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         paddingLeft: 20,
         paddingRight: 20,
         backgroundColor: theme.colors.themeBgColor,
