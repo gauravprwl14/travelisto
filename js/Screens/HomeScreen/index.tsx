0cceb51804c1e17ceb90675baa172b7c91842e97
gauravprwl14 <gauravprwl14@gmail.com>
2019-03-05 04:26:31 +0530

    added the example to show how to use the svg

--- js/Screens/HomeScreen/index.tsx
+++ js/Screens/HomeScreen/index.tsx
@@ -1,6 +1,28 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { Platform, StyleSheet, Text, View, StatusBar } from 'react-native'
+import Svg, {
+    Circle,
+    Ellipse,
+    G,
+    TSpan,
+    TextPath,
+    Path,
+    Polygon,
+    Polyline,
+    Line,
+    Rect,
+    Use,
+    Image,
+    Symbol,
+    Defs,
+    LinearGradient,
+    RadialGradient,
+    Stop,
+    ClipPath,
+    Pattern,
+    Mask
+} from 'react-native-svg'
 import Container from '../../Components/Container'
 import * as commonInterfaces from '../../utils/types/interface'
 import * as HomeScreenStyles from './styles'
@@ -19,6 +41,32 @@ class HomeScreen extends React.Component<HomeScreenProps> {
                     <Text style={HomeScreenStyles.StyleCreator.welcome}>
                         Travelisto
                     </Text>
+                </View>
+                <View
+                    style={[
+                        StyleSheet.absoluteFill,
+                        { alignItems: 'center', justifyContent: 'center' },
+                    ]}
+                >
+                    <Svg height='50%' width='50%' viewBox='0 0 100 100'>
+                        <Circle
+                            cx='50'
+                            cy='50'
+                            r='45'
+                            stroke='blue'
+                            strokeWidth='2.5'
+                            fill='green'
+                        />
+                        <Rect
+                            x='15'
+                            y='15'
+                            width='70'
+                            height='70'
+                            stroke='red'
+                            strokeWidth='2'
+                            fill='yellow'
+                        />
+                    </Svg>
                 </View>
             </Container>
         )
